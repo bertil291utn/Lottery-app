2f9583f87f43386a8265371e5b79708f7bb8dbef
Bertil Tandayamo <bertilt4@gmail.com>
2022-08-25 21:14:53 -0500

    setup hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,33 +1,41 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require('@nomiclabs/hardhat-ethers');
 require('@nomiclabs/hardhat-etherscan');
-require("@nomiclabs/hardhat-waffle")
-require("@appliedblockchain/chainlink-plugins-fund-link")
+require('@nomiclabs/hardhat-waffle');
+require('@appliedblockchain/chainlink-plugins-fund-link');
+require('hardhat-gas-reporter');
+require('solidity-coverage');
+require('hardhat-deploy');
 require('dotenv').config();
 module.exports = {
-  solidity: { compilers: [
-    {
-      version: "0.4.24",
-    },
-    {
-      version: "0.8.3",
-    }
-  ]},
-  defaultNetwork: "hardhat",
+  solidity: {
+    compilers: [
+      {
+        version: '0.4.24',
+      },
+      {
+        version: '0.8.3',
+      },
+    ],
+  },
+  defaultNetwork: 'hardhat',
   networks: {
     hardhat: {
       forking: {
-        url: process.env.ALCHEMY_MAINNET_RPC_URL
+        url: process.env.ALCHEMY_MAINNET_RPC_URL,
       },
     },
     rinkeby: {
       url: process.env.ALCHEMY_RINKEBY_RPC_URL,
       // accounts: [`${process.env.WALLET_PK}`],
-      accounts:{ mnemonic: process.env.MNEMONIC}
-
+      accounts: { mnemonic: process.env.MNEMONIC },
     },
   },
   etherscan: {
-    apiKey: 'KGCR9NM6MYX9Z8KYBB8CJIUHYVTMEVAYBZ',
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: 'USD',
   },
 };
