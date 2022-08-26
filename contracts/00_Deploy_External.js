ae6cd4be4484254c88aa8ec49f7023082117cfcd
Bertil Tandayamo <bertilt4@gmail.com>
2022-08-25 23:13:37 -0500

    add deploy

---
+++ contracts/00_Deploy_External.js
@@ -0,0 +1,17 @@
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = await getChainId();
+
+  if (chainId === 31337) {
+  log('Local Network Detected, Deploying external contracts');
+  const linkToken = await deploy('LinkToken', { from: deployer, log: true });
+  await deploy('VRFCoordinatorMock', {
+    from: deployer,
+    log: true,
+    args: [linkToken.address],
+  });
+  }
+};
+
+module.exports.tags = ['all', 'mocks', 'main'];
---
+++ contracts/01_Deploy_Lottery.js
@@ -0,0 +1,52 @@
+const { config } = require("../config/chainlink.config");
+
+module.exports = async ({ getNamedAccounts, deployments, getChainId, ethers }) => {
+  const { deploy, get, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = await getChainId();
+  let linkToken;
+  let linkTokenAddress;
+  let VRFCoordinatorMock;
+  let vrfCoordinatorAddress;
+  let additionalMessage = "";
+
+  if (chainId === 31337) {
+    linkToken = await get("LinkToken");
+    VRFCoordinatorMock = await get("VRFCoordinatorMock");
+    linkTokenAddress = linkToken.address;
+    vrfCoordinatorAddress = VRFCoordinatorMock.address;
+    additionalMessage =
+      " --linkaddress " +
+      linkTokenAddress +
+      " --fundadmount " +
+      config[chainId].fundAmount;
+  } else {
+    linkTokenAddress = config[chainId].linkToken;
+    vrfCoordinatorAddress = config[chainId].vrfCoordinator;
+  }
+  const keyHash = config[chainId].keyHash;
+  const fee = config[chainId].fee;
+
+  const lottery = await deploy("Lottery", {
+    from: deployer,
+    args: [
+      vrfCoordinatorAddress,
+      linkTokenAddress,
+      keyHash,
+      ethers.utils.parseUnits(fee, 18),
+    ],
+    log: true,
+  });
+
+  log("Run the following command to fund contract with LINK:");
+  log(
+    "npx hardhat fund-link --contract " +
+      lottery.address +
+      " --network " +
+      config[chainId].name +
+      additionalMessage
+  );
+  log("----------------------------------------------------");
+};
+
+module.exports.tags = ["all", "main"];
